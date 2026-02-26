0e494bb6d3b1a492b639a75dc17eada5e01fcf92
himu0023 <himanshubisht3231@gmail.com>
2026-02-26 09:44:39 +0530

    till pratical 4

--- pratical4.cpp
+++ pratical4.cpp
@@ -3,32 +3,39 @@
 #include <math.h>
 #include <stdlib.h>
 int main() {
- int gd = DETECT, gm;
- initgraph(&gd, &gm, "C:\\Turboc3\\BGI");
- int x1 = 100, y1 = 100;
- int x2 = 400, y2 = 300;
- float dx, dy, steps;
- float x_inc, y_inc;
- float x = x1;
- float y = y1;
- dx = x2 - x1;
- dy = y2 - y1;
- // Calculate number of steps
- if (abs(dx) > abs(dy))
- steps = abs(dx);
- else
- steps = abs(dy);
- // Calculate increment values
- x_inc = dx / steps;
- y_inc = dy / steps;
- // Plot pixels
- for (int i = 0; i <= steps; i++) {
-     putpixel(round(x), round(y), WHITE);
- x = x + x_inc;
- y = y + y_inc;
- }
- outtextxy(150, 50, "Line using DDA Algorithm");
- getch();
- closegraph();
- return 0;
+    int gd = DETECT, gm;
+    initgraph(&gd, &gm, "C:\\Turboc3\\BGI");
+
+    int x1 = 10, y1 = 490;
+    int x2 = 10, y2 = 60;
+    
+    float dx, dy, steps;
+    float x_inc, y_inc;
+    
+    float x = x1;
+    float y = y1;
+    
+    dx = x2 - x1;
+    dy = y2 - y1;
+    
+    // Calculate number of steps
+    if (abs(dx) > abs(dy))
+      steps = abs(dx);
+    else
+      steps = abs(dy);
+    
+    // Calculate increment values
+    x_inc = dx / steps;
+    y_inc = dy / steps;
+    
+    // Plot pixels
+    for (int i = 0; i <= steps; i++) {
+        putpixel(round(x), round(y), RED);
+        x = x + x_inc;
+        y = y + y_inc;
+    }
+    outtextxy(150, 50, "Line using DDA Algorithm");
+    getch();
+    closegraph();
+    return 0;
 }
